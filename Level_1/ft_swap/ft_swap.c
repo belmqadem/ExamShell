cd91c99bf9904f5fb2a05ceea250af9a210656b7
Adil Bel Mqadem <adilfront3@gmail.com>
2024-06-03 05:11:00 +0100

    Level one

--- Level_1/ft_swap/ft_swap.c
+++ Level_1/ft_swap/ft_swap.c
@@ -6,16 +6,3 @@ void    ft_swap(int *a, int *b)
     *a = *b;
     *b = tmp;
 }
-
-/*
-#include <stdio.h>
-int main()
-{
-    int x = 13;
-    int y = 37;
-
-    printf("Before Swap: X = %d and Y = %d\n", x, y);
-    ft_swap(&x, &y);
-    printf("After Swap: X = %d and Y = %d\n", x, y);
-}
-*/
